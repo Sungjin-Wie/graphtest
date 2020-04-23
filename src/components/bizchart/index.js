4fc592c40366e7e5a088cc5bd35a0731231e4b47
Sungjin-Wie <tmdwls1203@gmail.com>
2020-04-23 17:16:24 +0900

    ,

--- src/components/bizchart/index.js
+++ src/components/bizchart/index.js
@@ -13,7 +13,10 @@ const BizChart = () => {
     <>
       <div>
         <div style={{ marginLeft: 30, fontSize: 20 }}>
-          <div> BizChart (built with G2.js), 종류가 상당히 많음</div>
+          <div>
+            BizChart (built with G2.js), 종류가 상당히 많음, 기본 생성
+            애니메이션 존재
+          </div>
           <a href='https://bizcharts.net/product/bizcharts/gallery'>바로가기</a>
         </div>
         <Chart Component={LineHistMixed} name='Histogram with Line Chart' />
@@ -24,14 +27,7 @@ const BizChart = () => {
       <div>
         <Chart Component={Radar} name='Radar Chart' />
         <Chart Component={CurvedLine} name='Curved Line Chart' />
-        {/* <Chart Component={PieChart} name='Pie Chart' /> */}
       </div>
-      {/* <div style={{ clear: 'both' }}></div>
-      <div>
-        <Chart Component={RadarChart} name='Radar Chart' />
-        <Chart Component={RadialBarChart} name='Radial Bar Chart' />
-        <Chart Component={WithToolTip} name='Pie Chart' />
-      </div> */}
     </>
   );
 };
